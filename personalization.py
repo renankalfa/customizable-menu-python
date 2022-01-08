d976b7592527e6a6981a1f956b4cb9326b3f6a5c
Renan Barbosa <renansantosmath@gmail.com>
2022-01-08 20:32:47 -0300

    começo da personalização do menu

--- personalization.py
+++ personalization.py
@@ -4,5 +4,9 @@ from visualstratament import *
 def name_menu():
     formatText('MENU PERSONALIZATION')
     name = read_oneName('Name of your menu: ', 2)
-    formatText('MENU STYLE')
+    return name
 
+
+def style_menu():
+    formatText('STYLE PERSONALIZATION')
+    style_option = int(input('Style: '))
