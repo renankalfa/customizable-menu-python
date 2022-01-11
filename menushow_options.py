249c3a6d99721cb28aee590389295bb13fb08a46
Renan Barbosa <renansantosmath@gmail.com>
2022-01-11 12:36:07 -0300

    list for li name

--- menushow_options.py
+++ menushow_options.py
@@ -5,6 +5,6 @@ def showMenu_head(name_menu='no name', n=0):
     formatText(name_menu.upper(), n)
 
 
-def showMenu_options(*list):
-    for pos, value in enumerate(list):
+def showMenu_options(*li):
+    for pos, value in enumerate(li):
         print(f'\033[33m{pos}\033[m - \033[36m{value}\033[m')
